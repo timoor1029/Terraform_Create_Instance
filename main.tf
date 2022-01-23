f0633001a66ca393124810d90315dd9d159da50e
timoor1029 <55950556+timoor1029@users.noreply.github.com>
2022-01-23 12:38:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,132 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIATDRUQARCN35GX4F6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
-
-resource "aws_vpc" "first-vpc" {
+#Virtual private cloud (VPC) — A virtual network dedicated to your AWS account.
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "production"
+      Name = "production"
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
+
+#Internet gateway — A gateway that you attach to your VPC to enable communication between resources in your VPC and the internet.
+resource "aws_internet_gateway" "webGW" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+#Route table — A set of rules, called routes, that are used to determine where network traffic is directed.
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.webGW.id
+  }
 
   tags = {
-    Name = "prod-subnet"
+    Name = "Prod"
+  }
+}
+
+#Subnet — A range of IP addresses in your VPC.
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "prod-subnet"
+    }
+}
+
+#Associate Subnet with Route Table.
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+#Create Security Group to Allow Traffic 22.80.443.
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+    ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+      ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+#create network interface with an ip from the subnet from subnet we have created.
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+#assign an elastic IP to the network onterface.
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on = [aws_internet_gateway.webGW]
+}
+
+#create Ubuntu server and install/enable apache2
+resource "aws_instance" "web-server-instance" {
+  ami = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-1a"
+  key_name = "Terrafrom_Key"
+
+  network_interface {
+
+    device_index = 0
+    network_interface_id = aws_network_interface.web-server-nic.id
+  }
+
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo bash -c 'echo The best automation tool > /var/www/html/index.html'
+                EOF
+  tags = {
+    Name = "web-server"
   }
 }
